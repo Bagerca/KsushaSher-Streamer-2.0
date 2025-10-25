ff46fcc15ac230b5ccc02c790221c16782b5349e
Bagerca <bagrationbebia@gmail.com>
2025-10-26 00:17:14 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Main initialization file
 document.addEventListener('DOMContentLoaded', function() {
     // Initialize all modules
-    ThemeManager.init();
-    CosmicAnimation.init();
-    ScheduleManager.init();
-    MediaCards.init();
-    StatsRadar.init();
-    AudioVisualizer.init();
-    ReptileCursor.init();
+    if (typeof ThemeManager !== 'undefined') ThemeManager.init();
+    if (typeof CosmicAnimation !== 'undefined') CosmicAnimation.init();
+    if (typeof ScheduleManager !== 'undefined') ScheduleManager.init();
+    if (typeof MediaCards !== 'undefined') MediaCards.init();
+    if (typeof StatsRadar !== 'undefined') StatsRadar.init();
+    if (typeof AudioVisualizer !== 'undefined') AudioVisualizer.init();
+    if (typeof ReptileCursor !== 'undefined') ReptileCursor.init();
     
     console.log('Ksusha Sher website initialized!');
 });
